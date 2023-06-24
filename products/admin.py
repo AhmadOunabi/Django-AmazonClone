231f4f2a61212599f5a644b45038d53286273b4a
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-06-24 20:06:09 +0200

    Customize Adminpanel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from products.models import Product,Brand,Review,ProductImages
 
+
+class InlineProductImage(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ['name','brand','quantity','price']
+    list_filter=['brand','quantity','price']
+    inlines=[InlineProductImage]
+    
 # Register your models here.
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Brand)
+admin.site.register(Review)
+admin.site.register(ProductImages)
