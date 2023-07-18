3fed61cff76a0f12452af06116d7c7aebfe0672b
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-07-18 22:23:07 +0200

    add brands

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',include('products.urls') ),
     path('summernote/', include('django_summernote.urls')),
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
