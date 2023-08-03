fd649b143d61091605a3f0be2c7ae9676764ac99
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-08-03 22:00:18 +0200

    Backend HomePage

--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .models import Company
-# Create your views here.
-
+from products.models import Product , Brand , Review
 
 def home(request):
+    brands = Brand.objects.all()[:10]
+    products= Product.objects.all()[:10]
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    new_products = Product.objects.filter(flag='New')[:10]
+    reviews = Review.objects.all()
     
-    return render(request,'settings/home.html')
+    
+    return render(request,'settings/home.html',{
+        'brands':brands , 
+        'products':products,
+        'sale_products':sale_products , 
+        'feature_products':feature_products,
+        'new_products':new_products , 
+        'reviews':reviews
+    })
