0c19369577f448b03b21438a8359840a27a9384b
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-03 16:51:51 +0200

    add

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+ORDER_STATUS=(
+    ('Recieved','Recieved'),
+    ('Processed','Processed'),
+    ('Shipped','Shipped'),
+    ('Delivered','Delivered'),
+)
+
+class Orders(models.Model):
+    user=models.ForeignKey(User, related_name='order_owner',on_delete=models.SET_NULL,null=True)
+    status= models.CharField(max_length=12, choices=ORDER_STATUS)
+    
