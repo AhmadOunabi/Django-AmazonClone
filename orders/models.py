44af2c7a723c081db9fa2a5b8c29acfa81090c5d
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-04 00:21:53 +0200

    OrdersApp Begin Add Orders Model1 -Orders-

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from functions.code_generator import generate_code
+from accounts.models import Address
+from products.models import Product
+
 
 # Create your models here.
 
@@ -10,7 +15,16 @@ ORDER_STATUS=(
     ('Delivered','Delivered'),
 )
 
-class Orders(models.Model):
-    user=models.ForeignKey(User, related_name='order_owner',on_delete=models.SET_NULL,null=True)
-    status= models.CharField(max_length=12, choices=ORDER_STATUS)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User,related_name='order_owner',on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=12 , choices=ORDER_STATUS)
+    code = models.CharField(max_length=10,default=generate_code)
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True,blank=True)
+    total = models.FloatField()
+    delivery_location = models.ForeignKey(Address , related_name='order_address',on_delete=models.SET_NULL,null=True,blank=True)
     
+    def __str__(self):
+        return str(self.user)
