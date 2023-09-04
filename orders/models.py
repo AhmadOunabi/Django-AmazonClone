1797f02c2d6e5ec7c3f22bf31c83492753622c73
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-04 02:02:12 +0200

    Add Models Cart , CartDetails , Coupons

--- orders/models.py
+++ orders/models.py
@@ -35,4 +35,40 @@ class OrderDetail(models.Model):
     product=models.ForeignKey(Product,on_delete=models.SET_NULL,related_name='orderdetail_product')
     quantity=models.IntegerField()
     price=models.FloatField()
+    def __str__(self):
+        return str(self.order)
 
+
+CART_STATUS=(
+    ('Inprogress','Inprogress'),
+    ('Completed','Completed'),
+)
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,related_name='cart_owner',on_delete=models.SET_NULL)
+    status=models.CharField(max_length=20,choices=CART_STATUS)
+    def __str__(self):
+        return str(self.user)
+
+
+class CartDetail(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='cart_detail')
+    product= models.ForeignKey(Product,related_name='cartdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
+    quantity=models.IntegerField()
+    total=models.FloatField(null=True,blank=True)
+    
+    def save(self,*args,**kwargs):
+        self.total=self.quantity * self.product.price
+        super(CartDetail,self).save(*args,**kwargs)
+    def __str__(self):
+        return str(self.product)
+
+class Coupons(models.Model):
+    code = models.CharField(max_length=20)
+    start_date=models.DateTimeField(default=timezone.now)
+    end_date=models.DateTimeField(default=timezone.now)
+    discount=models.FloatField()
+    quantity=models.IntegerField()
+    
+    def __str__(self):
+        return str(self.code)
