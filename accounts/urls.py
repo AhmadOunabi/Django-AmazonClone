af3152c2a41790617b505f8c3a6463a67e38e18d
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-13 22:57:40 +0200

    Add

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import signup, user_activate,profile
 
 urlpatterns = [
-    path('profile', profile),
+    path('profile/', profile),
     path('signup', signup),
     path('<slug:username>/activate', user_activate ),
 ]
