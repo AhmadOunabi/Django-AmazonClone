5df15231bd937a5348a5663bf5e1e77e8223d07b
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-19 21:21:49 +0200

    Show Product Reviews and Images in Product Details

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
-from .models import Product , Brand
+from .models import Product , Brand, Review, ProductImages
 from django.db.models.aggregates import Avg
 
 
 
+
+class BrandListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Brand
+        fields='__all__'
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ProductImages
+        fields= ['image']
+
+
+ 
 class ProductListSerializer(serializers.ModelSerializer):
-    
+    brand=serializers.StringRelatedField()                  # To show the brand name instead of the Brand Number 
     review_count = serializers.SerializerMethodField()
     avg_rate= serializers.SerializerMethodField()
     class Meta:
@@ -21,19 +35,22 @@ class ProductListSerializer(serializers.ModelSerializer):
         avg=product.product_review.aggregate(rate_avg=Avg('rate'))
         return(avg['rate_avg']) if avg['rate_avg'] else 0
         
-        
+
+
+class ProductReviewSerializer(serializers.ModelSerializer):
+    user=serializers.StringRelatedField()                     # To show the USER name instead of the Brand Number
+    class Meta:
+        model= Review
+        fields=['user','review','rate']
 
 class ProductDetailSerializer(serializers.ModelSerializer):
+    image=ProductImageSerializer(source='product_images', many=True)
+    reviews=ProductReviewSerializer(source='product_review', many=True)
+    brand=serializers.StringRelatedField()                     # To show the brand name instead of the Brand Number 
     class Meta:
         model= Product
         fields= '__all__'
 
-
-
-class BrandListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Brand
-        fields='__all__'
 
 
 
