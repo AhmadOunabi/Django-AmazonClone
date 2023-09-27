c81e16f5ddaa2148a668b18370a534f6ba8c698a
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-27 10:48:11 +0200

    Delete Product from the Cart

--- orders/api.py
+++ orders/api.py
@@ -12,4 +12,16 @@ class CartDetailCreateAPI(generics.GenericAPIView):
         cart,created=Cart.objects.get_or_create(user=user,status='Inprogress')
         data=CartSerializer(cart).data
         return Response({'cart':data})
+    
+    def post(self,request,*args, **kwargs):
+        user=User.objects.get(username=self.kwargs['username'])
+    
+    def delete(self,request,*args, **kwargs):
+        user=User.objects.get(username=self.kwargs['username'])
+        product= Product.objects.get(id=request.data['product_id'])
+        cart=Cart.objects.get(user=user,status='Inprogress')
+        cart_detail=CartDetail.objects.get(cart=cart,product=product)
+        cart_detail.delete()
+        return Response({'status':200, 'message': 'Product Deleted Successfully'})
+        
         
