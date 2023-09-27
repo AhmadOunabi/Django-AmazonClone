f246033ed881095a7eb5b6da74395751e0b41b2c
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-09-27 11:10:30 +0200

    Add or Update Product in the Cart

--- orders/api.py
+++ orders/api.py
@@ -7,14 +7,22 @@ from products.models import Product
 
 
 class CartDetailCreateAPI(generics.GenericAPIView):
-    def get(self,request,*args,**kwargs):
+    def get(self,request,*args,**kwargs):                       # get or create Cart based on user
         user=User.objects.get(username=self.kwargs['username'])                     #we didnt use request.user because the cart can be added without user login
         cart,created=Cart.objects.get_or_create(user=user,status='Inprogress')
         data=CartSerializer(cart).data
         return Response({'cart':data})
     
-    def post(self,request,*args, **kwargs):
+    def post(self,request,*args, **kwargs):                     # craete or update products in the Cart
         user=User.objects.get(username=self.kwargs['username'])
+        product=Product.object.get(id=request.data['product_id'])
+        cart=Cart.object.get(user=user,status='Inprogress')
+        cart_detail,created=CartDetail.objects.get_or_create(cart=cart,product=product)
+        quantity=int(request.data['quantity'])
+        cart_detail.quantity=quantity
+        cart_detail.total= quantity*product.price
+        cart_detail.save()
+        return Response({'status':200,'message':'Product Added Successfully'})
     
     def delete(self,request,*args, **kwargs):
         user=User.objects.get(username=self.kwargs['username'])
